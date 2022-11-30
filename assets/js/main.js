e661558a1fccf4f4dac8aa63d43043a4eec11601
Искандар <105116560+isikdev@users.noreply.github.com>
2022-11-30 11:09:03 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let menu = $('.nav__links-mb');
+$('.toggleMenu').on("click", function () {
+    menu.toggleClass('nav__links-mb-active');
+});
+// $('.carousel').carousel({
+//     interval: 1000
+// })
