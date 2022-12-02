03fba18adb38298c9a57abd3a5ff8656b345855c
Искандар <105116560+isikdev@users.noreply.github.com>
2022-12-02 18:37:57 +0500

    product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,10 @@ $('.toggleMenu').on("click", function () {
 // $('.carousel').carousel({
 //     interval: 1000
 // })
+$('.product__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.carousel-control-prev'),
+    nextArrow: $('.carousel-control-next'),
+});
